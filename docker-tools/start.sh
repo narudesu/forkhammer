309271d597b2bd3b2d85be7a8446c912880edbda
Matej Smid <narudesu@yahoo.com>
2026-07-06 00:59:52 +0200

    fix worker start

--- docker-tools/start.sh
+++ docker-tools/start.sh
@@ -5,7 +5,7 @@ opencode serve --hostname "0.0.0.0" --port "8000" &
 OPENCODE_PID=$!
 
 cd /app/forkhammer
-bun run /app/forkhammer/src/cli.ts start-worker &
+bun run /app/forkhammer/src/run-cli.ts start-worker &
 WORKER_PID=$!
 
 cleanup() {
